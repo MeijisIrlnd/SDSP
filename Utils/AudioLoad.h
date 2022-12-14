1340d68e0e18833f4f7926dbb165ecbbcd59d8e4
MeijisIrlnd <syl.morrison.dev@gmail.com>
2022-12-14 21:28:54 +0000

    New audio load stuff

--- Utils/AudioLoad.h
+++ Utils/AudioLoad.h
@@ -6,7 +6,7 @@ namespace SDSP::Utils
     static inline double loadAudioFile(juce::AudioFormatManager& formatManager, const juce::File& toLoad, juce::AudioBuffer<float>& destBuffer) 
     { 
         std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(toLoad));
-        destBuffer.setSize(2, static_cast<int>(reader->lengthInSamples));
+        destBuffer.setSize(reader->lengthInSamples, static_cast<int>(reader->lengthInSamples));
         reader->read(&destBuffer, 0, static_cast<int>(reader->lengthInSamples), 0, true, true);
         return reader->sampleRate;
     }
@@ -15,6 +15,23 @@ namespace SDSP::Utils
     {
         juce::AudioBuffer<float> loadBuffer;
         auto loadedSampleRate = loadAudioFile(formatManager, toLoad, loadBuffer);
+        destBuffer = resampleAudioBuffer(loadBuffer, loadedSampleRate, sampleRate);
+    }
+
+    static inline double loadFromMemory(juce::AudioFormatManager& formatManager, const void* data, int size, juce::AudioBuffer<float>& destBuffer) 
+    { 
+        std::unique_ptr<juce::MemoryInputStream> ip(new juce::MemoryInputStream(data, size, false));
+        std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(std::move(ip)));
+        if (reader.get() == nullptr) return -1;
+        destBuffer.setSize(reader->numChannels, static_cast<int>(reader->lengthInSamples), false);
+        reader->read(&destBuffer, 0, static_cast<int>(reader->lengthInSamples), 0, true, true);
+        return reader->sampleRate;
+    }
+
+    static inline void loadFromMemoryWithResample(juce::AudioFormatManager& formatManager, const void* data, int size, const double sampleRate, juce::AudioBuffer<float>& destBuffer)
+    {
+        juce::AudioBuffer<float> loadBuffer;
+        auto loadedSampleRate = loadFromMemory(formatManager, data, size, loadBuffer);
         destBuffer = resampleAudioBuffer(loadBuffer, loadedSampleRate, sampleRate);
     }
 }
