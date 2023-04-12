d6d805e5b10f401ced4a36186745c2a01e2adc71
MeijisIrlnd <syl.morrison.dev@gmail.com>
2023-04-12 17:14:15 +0100

    Fuck them spaces

--- Filters/FilterHelpers.h
+++ Filters/FilterHelpers.h
@@ -7,7 +7,7 @@
 namespace SDSP::Filters {
 //https://www.musicdsp.org/en/latest/Filters/85-1st-and-2nd-order-pink-noise-filters.html
     constexpr static inline std::array<double, 6> pinking() {
-        return std::array < double, 6 > {0.04957526213389, -0.06305581334498, 0.01483220320740, 1.00000000000000, -1.80116083982126, 0.80257737639225};
+        return std::array<double, 6>{0.04957526213389, -0.06305581334498, 0.01483220320740, 1.00000000000000, -1.80116083982126, 0.80257737639225};
     }
 }
 
