5c431054de6494bc08fdd6029649c5634367ca7e
MeijisIrlnd <syl.morrison.dev@gmail.com>
2023-05-16 17:35:27 +0100

    C++20 preprocessors

--- Helpers/Helpers.h
+++ Helpers/Helpers.h
@@ -14,21 +14,32 @@ namespace SDSP::Helpers
     template<class T, size_t N>
     struct is_std_array<typename std::array<T, N> > : std::true_type { };
 
+#if __cplusplus >= 202002L
     template<typename T>
     concept NumericArray =
         (SDSP::Helpers::is_std_array<T>::value && std::is_arithmetic<typename T::value_type>::value) || std::is_array<T>::value;
-
+#endif
+#if __cplusplus >= 202002L
     template<typename T> requires NumericArray<T>
+#else
+    template<typename T>
+#endif
     SDSP_UNUSED static inline void zero_array(T& data) {
         std::memset(data.data(), 0.0f, sizeof(typename T::value_type) * data.size());
     }
-
+#if __cplusplus >= 202002L
     template<typename T> requires NumericArray<T>
+#else
+    template<typename T>
+#endif
     SDSP_UNUSED static inline void copy_array(T& dest, T& src) {
         std::memcpy(dest.data(), src.data(), sizeof(typename T::value_type) * dest.size());
     }
-
+#if __cplusplus >= 202002L
     template<typename T> requires NumericArray<T>
+#else
+    template<typename T>
+#endif
     SDSP_UNUSED static inline void fill_array(T& dest, typename T::value_type value) {
         std::fill(dest.begin(), dest.end(), value);
     }
