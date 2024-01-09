91ced9d5d25cef694cdbc3cd65d7945fc0ea7c4c
MeijisIrlnd <syl.morrison.dev@gmail.com>
2024-01-09 18:31:54 +0000

    All warnings fixed MSVC

--- Types/CircularBuffer.h
+++ Types/CircularBuffer.h
@@ -16,47 +16,39 @@
 #include <juce_audio_basics/juce_audio_basics.h>
 #include <juce_core/juce_core.h>
 
-namespace SDSP
-{
+namespace SDSP {
     /// <summary>
     /// Mono circular buffer with a max size of 5 seconds, with tape delay style interpolation on delay time change
     /// </summary>
     /// <typeparam name="T"></typeparam>
     template <typename T>
-    class [[maybe_unused]] CircularBuffer
-    {
+    class [[maybe_unused]] CircularBuffer {
     public:
-
         CircularBuffer<T>(double interp, double sampleRate)
             : interpolationTimeMs(static_cast<int>(interp)),
-            buffer(new T[static_cast<size_t>(sampleRate * 5)]),
-            recordHead(buffer),
-            playbackHead(buffer)
-        {            
+              buffer(new T[static_cast<size_t>(sampleRate * 5)]),
+              recordHead(buffer),
+              playbackHead(buffer) {
             std::memset(buffer, 0, static_cast<size_t>(sampleRate * 5) * sizeof(T));
         }
 
         explicit CircularBuffer<T>(double interp) : interpolationTimeMs(static_cast<int>(interp)) {
-
         }
 
-        CircularBuffer<T>() : interpolationTimeMs(500) { }
+        CircularBuffer<T>() : interpolationTimeMs(500) {}
 
         CircularBuffer<T>(const CircularBuffer<T>& other) : interpolationTimeMs(other.interpolationTimeMs), m_sampleRate(other.m_sampleRate) {
-
         }
 
         CircularBuffer<T>(CircularBuffer<T>&& other) noexcept : interpolationTimeMs(other.interpolationTimeMs), m_sampleRate(other.m_sampleRate) {
-
         }
 
-        ~CircularBuffer()
-        {
+        ~CircularBuffer() {
             delete[] buffer;
         }
 
         CircularBuffer<T>& operator=(const CircularBuffer<T>& other) {
-            if(&other != this) {
+            if (&other != this) {
                 interpolationTimeMs = other.interpolationTimeMs;
                 m_sampleRate = other.m_sampleRate;
                 m_maxDelaySeconds = other.m_maxDelaySeconds;
@@ -70,25 +62,24 @@ namespace SDSP
             m_maxDelaySeconds = other.m_maxDelaySeconds;
             return *this;
         }
-        
+
         // REFACTOR: what the fuck hahah
-        void prepare(SDSP_UNUSED int spb, double sampleRate)
-        {
+        void prepare(SDSP_UNUSED int spb, double sampleRate) {
             m_sampleRate = sampleRate;
             bufferSize = static_cast<size_t>(m_maxDelaySeconds * sampleRate);
 
             delete[] buffer;
             buffer = new T[bufferSize];
             std::memset(buffer, 0, bufferSize * sizeof(T));
-            
+
             recordHead = buffer;
             playbackHead = buffer;
-            
+
             interpolator.prepare(sampleRate);
             interpolator.set(0, 0.5 * sampleRate, interpolationTimeMs);
         }
 
-        void setMaxDelaySeconds(const double maxDelaySeconds) { 
+        void setMaxDelaySeconds(const double maxDelaySeconds) {
             std::scoped_lock<std::mutex> sl(m_mutex);
             m_maxDelaySeconds = maxDelaySeconds;
             bufferSize = m_maxDelaySeconds * m_sampleRate;
@@ -99,15 +90,12 @@ namespace SDSP
             playbackHead = buffer;
         }
 
-        void clear()
-        {
+        void clear() {
             std::memset(buffer, 0, bufferSize * sizeof(T));
             playbackHead = buffer;
             recordHead = buffer;
-
         }
-        void setDelay(int m)
-        {
+        void setDelay(int m) {
             m = m < 0 ? 0 : m;
             m = m > m_maxDelaySeconds * m_sampleRate ? static_cast<int>(m_maxDelaySeconds * m_sampleRate) : m;
             m_delaySamples = m;
@@ -116,33 +104,29 @@ namespace SDSP
 
         [[maybe_unused]] [[nodiscard]] int getDelay() const { return m_delaySamples; }
 
-        SDSP_UNUSED void setInterpolationRate(double interpTimeMs)
-        {
+        SDSP_UNUSED void setInterpolationRate(double interpTimeMs) {
             interpolationTimeMs = static_cast<int>(interpTimeMs);
         }
 
-        SDSP_INLINE T getNextSample(T in)
-        {
+        SDSP_INLINE T getNextSample(T in) {
             std::scoped_lock<std::mutex> sl(m_mutex);
-            if(interpolationTimeMs != 0) {
-                recordHead = playbackHead - (int) interpolator.process();
-            }
-            else {
+            if (interpolationTimeMs != 0) {
+                recordHead = playbackHead - (int)interpolator.process();
+            } else {
                 recordHead = playbackHead - m_delaySamples;
             }
             while (recordHead < buffer) {
                 recordHead += bufferSize;
             }
-            
+
             T y;
             *playbackHead = in;
             ++playbackHead;
             y = *recordHead;
             ++recordHead;
-            if ((playbackHead - buffer) >= bufferSize) playbackHead -= bufferSize;
-            if ((recordHead - buffer) >= bufferSize) recordHead -= bufferSize;
+            if ((playbackHead - buffer) >= static_cast<std::ptrdiff_t>(bufferSize)) playbackHead -= bufferSize;
+            if ((recordHead - buffer) >= static_cast<std::ptrdiff_t>(bufferSize)) recordHead -= bufferSize;
             return y;
-
         }
 
     private:
@@ -155,8 +139,7 @@ namespace SDSP
         double m_sampleRate{};
         size_t bufferSize{};
         int interpolationTimeMs = 500;
-        double m_maxDelaySeconds{5};
+        double m_maxDelaySeconds{ 5 };
         int m_delaySamples{ 0 };
-
     };
-}
+} // namespace SDSP
--- Types/MultitapCircularBuffer.h
+++ Types/MultitapCircularBuffer.h
@@ -1,86 +1,82 @@
 #include <juce_audio_basics/juce_audio_basics.h>
 // TODO: TEST AND MODULATE
-namespace SDSP 
-{ 
-    template<int NTAPS>
-    class SDSP_UNUSED MultitapCircularBuffer
-    { 
-        public: 
-            MultitapCircularBuffer() { 
-                std::fill(m_delayTimes.begin(), m_delayTimes.end(), 0.0f);
+namespace SDSP {
+    template <int NTAPS>
+    class [[maybe_unused]] MultitapCircularBuffer {
+    public:
+        MultitapCircularBuffer() {
+            std::fill(m_delayTimes.begin(), m_delayTimes.end(), 0);
+        }
+        void prepareToPlay(SDSP_UNUSED int samplesPerBlockExpected, double sampleRate) {
+            m_sampleRate = sampleRate;
+            m_buffer.resize(static_cast<size_t>(static_cast<double>(m_maxDelaySeconds) * sampleRate));
+            std::fill(m_buffer.begin(), m_buffer.end(), 0.0f);
+            std::fill(m_read.begin(), m_read.end(), 0);
+            for (size_t tap = 0; tap < static_cast<size_t>(NTAPS); tap++) {
+                m_smoothedDelayTimes[tap].reset(sampleRate, 0.5f);
+                m_smoothedDelayTimes[tap].setCurrentAndTargetValue(static_cast<float>(m_delayTimes[tap]));
             }
-            void prepareToPlay(SDSP_UNUSED int samplesPerBlockExpected, double sampleRate) {
-                m_sampleRate = sampleRate;
-                m_buffer.resize(m_maxDelaySeconds * sampleRate);
-                std::fill(m_buffer.begin(), m_buffer.end(), 0.0f);
-                std::fill(m_read.begin(), m_read.end(), 0.0f);
-                for(auto tap = 0; tap < NTAPS; tap++) { 
-                    m_smoothedDelayTimes[tap].reset(sampleRate, 0.5f);
-                    m_smoothedDelayTimes[tap].setCurrentAndTargetValue(m_delayTimes[tap]);
+            m_hasBeenPrepared = true;
+        }
+
+        std::array<float, NTAPS> processSample(float x) {
+            juce::FloatVectorOperations::fill(m_output.data(), 0.0f, NTAPS);
+            while (m_write >= m_buffer.size()) {
+                m_write -= static_cast<int>(m_buffer.size());
+            }
+
+            m_buffer[m_write] = x;
+            for (size_t tap = 0; tap < static_cast<size_t>(NTAPS); ++tap) {
+                m_read[tap] = m_write - static_cast<int>(m_smoothedDelayTimes[tap].getNextValue());
+                // m_read[tap] = m_write - m_delayTimes[tap];
+                while (m_read[tap] < 0) {
+                    m_read[tap] += static_cast<int>(m_buffer.size());
                 }
-                m_hasBeenPrepared = true;
-
-            }
-
-            std::array<float, NTAPS> processSample(float x) { 
-                juce::FloatVectorOperations::fill(m_output.data(), 0.0f, NTAPS);
-                while(m_write >= m_buffer.size()) { 
-                    m_write -= m_buffer.size();
-                }
-
-                m_buffer[m_write] = x;
-                for(auto tap = 0; tap < NTAPS; tap++) { 
-                    m_read[tap] = m_write - m_smoothedDelayTimes[tap].getNextValue();
-                    //m_read[tap] = m_write - m_delayTimes[tap];
-                    while(m_read[tap] < 0) { 
-                        m_read[tap] += m_buffer.size();
-                    }
-                    //while(m_read[tap] < m_buffer.size()) { 
-                    //    m_read[tap] += m_buffer.size();
-                    //}
-                    m_output[tap] = m_buffer[m_read[tap]];
-                    ++m_read[tap];
-                    if(m_read[tap] >= m_buffer.size() - 1) { 
-                        m_read[tap] -= m_buffer.size();
-                    }
-                }
-                ++m_write;
-
-                return m_output;
-            }
-
-            float releaseResources() { 
-
-            }
-
-            SDSP_UNUSED void setMaximumDelaySeconds(float maxDelaySeconds) {
-                m_maxDelaySeconds = maxDelaySeconds;
-                if(m_hasBeenPrepared) { 
-                    m_buffer.resize(m_maxDelaySeconds * m_sampleRate);
-                    std::fill(m_buffer.begin(), m_buffer.end(), 0.0f);
+                // while(m_read[tap] < m_buffer.size()) {
+                //     m_read[tap] += m_buffer.size();
+                // }
+                m_output[tap] = m_buffer[m_read[tap]];
+                ++m_read[tap];
+                if (m_read[tap] >= static_cast<int>(m_buffer.size()) - 1) {
+                    m_read[tap] -= static_cast<int>(m_buffer.size());
                 }
             }
+            ++m_write;
 
-            void setDelayTime(int delayTime, int tap) { 
-                m_delayTimes[tap] = delayTime;
-                if(m_hasBeenPrepared) { 
-                    m_smoothedDelayTimes[tap].setTargetValue(delayTime);
-                }
-            }
+            return m_output;
+        }
 
-            void clearBuffers() {
+        float releaseResources() {
+        }
+
+        SDSP_UNUSED void setMaximumDelaySeconds(float maxDelaySeconds) {
+            m_maxDelaySeconds = maxDelaySeconds;
+            if (m_hasBeenPrepared) {
+                m_buffer.resize(m_maxDelaySeconds * m_sampleRate);
                 std::fill(m_buffer.begin(), m_buffer.end(), 0.0f);
             }
+        }
 
-        private:
-            bool m_hasBeenPrepared{false};
-            double m_sampleRate{};
-            std::vector<float> m_buffer;
-            int m_write = 0;
-            std::array<int, NTAPS> m_read;
-            std::array<int, NTAPS> m_delayTimes;
-            float m_maxDelaySeconds{2};
-            std::array<float, NTAPS> m_output;
-            std::array<juce::SmoothedValue<float>, NTAPS> m_smoothedDelayTimes;
-    };      
-}
+        void setDelayTime(int delayTime, int tap) {
+            m_delayTimes[tap] = delayTime;
+            if (m_hasBeenPrepared) {
+                m_smoothedDelayTimes[tap].setTargetValue(static_cast<float>(delayTime));
+            }
+        }
+
+        void clearBuffers() {
+            std::fill(m_buffer.begin(), m_buffer.end(), 0.0f);
+        }
+
+    private:
+        bool m_hasBeenPrepared{ false };
+        double m_sampleRate{};
+        std::vector<float> m_buffer;
+        int m_write = 0;
+        std::array<int, NTAPS> m_read;
+        std::array<int, NTAPS> m_delayTimes;
+        float m_maxDelaySeconds{ 2 };
+        std::array<float, NTAPS> m_output;
+        std::array<juce::SmoothedValue<float>, NTAPS> m_smoothedDelayTimes;
+    };
+} // namespace SDSP
