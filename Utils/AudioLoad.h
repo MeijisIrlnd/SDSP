6ae1eec1f6296f48cd32301d136d5f296ac284e6
MeijisIrlnd <syl.morrison.dev@gmail.com>
2024-04-09 16:31:41 +0100

    Fixed audioload

--- Utils/AudioLoad.h
+++ Utils/AudioLoad.h
@@ -1,27 +1,23 @@
-#pragma once 
+#pragma once
 #include <juce_core/juce_core.h>
 #include <juce_audio_formats/juce_audio_formats.h>
 #include "Resampling.h"
 #include "../Macros.h"
-namespace SDSP::Utils 
-{ 
-    SDSP_UNUSED static inline double loadAudioFile(juce::AudioFormatManager& formatManager, const juce::File& toLoad, juce::AudioBuffer<float>& destBuffer)
-    { 
+namespace SDSP::Utils {
+    SDSP_UNUSED static inline double loadAudioFile(juce::AudioFormatManager& formatManager, const juce::File& toLoad, juce::AudioBuffer<float>& destBuffer) {
         std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(toLoad));
-        destBuffer.setSize(reader->lengthInSamples, static_cast<int>(reader->lengthInSamples));
+        destBuffer.setSize(static_cast<int>(reader->numChannels), static_cast<int>(reader->lengthInSamples));
         reader->read(&destBuffer, 0, static_cast<int>(reader->lengthInSamples), 0, true, true);
         return reader->sampleRate;
     }
 
-    SDSP_UNUSED static inline void loadAudioFileWithResample(juce::AudioFormatManager& formatManager, const juce::File& toLoad, const double sampleRate, juce::AudioBuffer<float>& destBuffer)
-    {
+    SDSP_UNUSED static inline void loadAudioFileWithResample(juce::AudioFormatManager& formatManager, const juce::File& toLoad, const double sampleRate, juce::AudioBuffer<float>& destBuffer) {
         juce::AudioBuffer<float> loadBuffer;
         auto loadedSampleRate = loadAudioFile(formatManager, toLoad, loadBuffer);
         destBuffer = resampleAudioBuffer(loadBuffer, loadedSampleRate, sampleRate);
     }
 
-    SDSP_UNUSED static inline double loadFromMemory(juce::AudioFormatManager& formatManager, const void* data, int size, juce::AudioBuffer<float>& destBuffer)
-    { 
+    SDSP_UNUSED static inline double loadFromMemory(juce::AudioFormatManager& formatManager, const void* data, int size, juce::AudioBuffer<float>& destBuffer) {
         std::unique_ptr<juce::MemoryInputStream> ip(new juce::MemoryInputStream(data, size, false));
         std::unique_ptr<juce::AudioFormatReader> reader(formatManager.createReaderFor(std::move(ip)));
         if (reader.get() == nullptr) return -1;
@@ -30,10 +26,9 @@ namespace SDSP::Utils
         return reader->sampleRate;
     }
 
-    SDSP_UNUSED static inline void loadFromMemoryWithResample(juce::AudioFormatManager& formatManager, const void* data, int size, const double sampleRate, juce::AudioBuffer<float>& destBuffer)
-    {
+    SDSP_UNUSED static inline void loadFromMemoryWithResample(juce::AudioFormatManager& formatManager, const void* data, int size, const double sampleRate, juce::AudioBuffer<float>& destBuffer) {
         juce::AudioBuffer<float> loadBuffer;
         auto loadedSampleRate = loadFromMemory(formatManager, data, size, loadBuffer);
         destBuffer = resampleAudioBuffer(loadBuffer, loadedSampleRate, sampleRate);
     }
-}
+} // namespace SDSP::Utils
--- Utils/Resampling.h
+++ Utils/Resampling.h
@@ -1,35 +1,30 @@
-#pragma once 
+#pragma once
 #include <juce_core/juce_core.h>
 #include <juce_audio_basics/juce_audio_basics.h>
 #include "../Macros.h"
-namespace SDSP::Utils 
-{ 
-	//https://github.com/stekyne/PhaseVocoder/blob/master/DSP/Resample.h thank you again stekyne.
-    SDSP_UNUSED inline static void linearResample(const float* const input, const int originalSize, float* const newSignal, const int newSignalSize)
-    { 
-	    const auto lerp = [&](float v0, float v1, float t)
-	    {
-	    	return (1.f - t) * v0 + t * v1;
-	    };
+namespace SDSP::Utils {
+    // https://github.com/stekyne/PhaseVocoder/blob/master/DSP/Resample.h thank you again stekyne.
+    SDSP_UNUSED inline static void linearResample(const float* const input, const int originalSize, float* const newSignal, const int newSignalSize) {
+        const auto lerp = [&](float v0, float v1, float t) {
+            return (1.f - t) * v0 + t * v1;
+        };
 
-	    // If the original signal is bigger than the new size, condense the signal to fit the new buffer
-	    // otherwise expand the signal to fit the new buffer
-	    const auto scale = static_cast<float>(originalSize) / static_cast<float>(newSignalSize);
-	    float index = 0.f;
+        // If the original signal is bigger than the new size, condense the signal to fit the new buffer
+        // otherwise expand the signal to fit the new buffer
+        const auto scale = static_cast<float>(originalSize) / static_cast<float>(newSignalSize);
+        float index = 0.f;
 
-	    for (int i = 0; i < newSignalSize; ++i)
-	    {
-	    	const auto wholeIndex = (int)std::floor(index);
-	    	const auto fractionIndex = index - static_cast<float>(wholeIndex);
-	    	const auto sampleA = input[wholeIndex];
-	    	const auto sampleB = input[wholeIndex + 1];
-	    	newSignal[i] = lerp(sampleA, sampleB, fractionIndex);
-	    	index += scale;
-	    }
+        for (int i = 0; i < newSignalSize; ++i) {
+            const auto wholeIndex = (int)std::floor(index);
+            const auto fractionIndex = index - static_cast<float>(wholeIndex);
+            const auto sampleA = input[wholeIndex];
+            const auto sampleB = input[wholeIndex + 1];
+            newSignal[i] = lerp(sampleA, sampleB, fractionIndex);
+            index += scale;
+        }
     }
 
-    inline static juce::AudioBuffer<float> resampleAudioBuffer(juce::AudioBuffer<float>& toResample, double originalSampleRate, double newSampleRate) 
-    { 
+    inline static juce::AudioBuffer<float> resampleAudioBuffer(juce::AudioBuffer<float>& toResample, double originalSampleRate, double newSampleRate) {
         juce::AudioBuffer<float> resampled;
         double ratio = originalSampleRate / newSampleRate;
         resampled.setSize(toResample.getNumChannels(), static_cast<int>(toResample.getNumSamples() / ratio));
@@ -42,4 +37,4 @@ namespace SDSP::Utils
         }
         return resampled;
     }
-}
+} // namespace SDSP::Utils
